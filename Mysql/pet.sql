e7e45e0abce74d348188e337798ad83711832f24
POOMESH-19 <149484652+POOMESH-19@users.noreply.github.com>
2024-04-30 20:41:38 +0530

    Add files via upload

---
+++ Mysql/pet.sql
@@ -0,0 +1,87 @@
+
+use pet;
+
+
+create table dog1(
+	 pet_name varchar(50),
+     category varchar(50),
+     breed varchar(50),
+     gender varchar(50),
+     age varchar(50),
+     owner_name varchar(50),
+     price float
+);
+
+
+
+
+
+create table cat1(
+     pet_name varchar(50),
+     category varchar(50),
+     breed varchar(50),
+     gender varchar(50),
+     age varchar(50),
+     owner_name varchar(50),
+     price float
+);
+
+
+       
+create table rabbit1(
+     pet_name varchar(50),
+     category varchar(50),
+     breed varchar(50),
+     gender varchar(50),
+     age varchar(50),
+     owner_name varchar(50),
+     price float
+);
+
+
+
+  
+create table turtle1(
+     pet_name varchar(50),
+     category varchar(50),
+     breed varchar(50),
+     gender varchar(50),
+     age varchar(50),
+     owner_name varchar(50),
+     price float
+);
+
+
+
+
+       
+create table parrot1(
+	pet_name varchar(50),
+     category varchar(50),
+     breed varchar(50),
+     gender varchar(50),
+     age varchar(50),
+     owner_name varchar(50),
+     price float
+);
+
+
+  
+create table fish1(
+	pet_name varchar(50),
+     category varchar(50),
+     breed varchar(50),
+     gender varchar(50),
+     age varchar(50),
+     owner_name varchar(50),
+     price float
+);
+
+
+ 
+
+
+
+
+
+
